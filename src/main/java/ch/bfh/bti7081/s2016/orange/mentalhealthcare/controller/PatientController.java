712391e6944788bb19053399ddca90e07c67d521
schne5 <elisa.schnabel@gmx.ch>
2016-05-20 15:46:12 +0200

    added controller patient

---
+++ src/main/java/ch/bfh/bti7081/s2016/orange/mentalhealthcare/controller/PatientController.java
@@ -0,0 +1,19 @@
+package ch.bfh.bti7081.s2016.orange.mentalhealthcare.controller;
+
+import ch.bfh.bti7081.s2016.orange.mentalhealthcare.model.Patient;
+
+public class PatientController {
+	
+	public void create(Patient p){
+		
+	}
+	
+	public void update(Patient p){
+		
+	}
+	
+	public void delete(Patient p){
+		
+	}
+
+}
