802359c502ea818b916bb0ba5adb11a9afc48130
schea2 <annakatharina.schenk@gmail.com>
2016-05-27 09:25:12 +0200

    Feldvalidierung

--- src/main/java/ch/bfh/bti7081/s2016/orange/mentalhealthcare/controller/PatientController.java
+++ src/main/java/ch/bfh/bti7081/s2016/orange/mentalhealthcare/controller/PatientController.java
@@ -16,12 +16,25 @@ public class PatientController {
 	}
 
 	public Patient update(Patient p) {
-		// TODO Validierungen
-		return repository.update(p);
+		if (isValid(p)) {
+			return repository.update(p);
+		}
+		// TODO Etwas sinnvolles
+		return null;
 	}
 
 	public void delete(Patient p) {
 		repository.delete(p.getId());
 	}
 
+	public boolean isValid(Patient p) {
+		boolean isValid = !"".equals(p.getName()) && null != p.getName()
+				&& p.getName().length() <= 50;
+		isValid &= !"".equals(p.getVorname()) && null != p.getVorname()
+				&& p.getName().length() <= 50;
+		isValid &= !"".equals(p.getSvNr()) && null != p.getSvNr()
+				&& p.getName().length() <= 25;
+		isValid &= null != p.getGebDatum();
+		return isValid;
+	}
 }
