6b34a54fd74d0875d6502652e374164cd77d4fc2
schas2 <schas2@users.noreply.github.com>
2016-05-29 11:30:34 +0200

    Change layout and margins

--- src/main/java/ch/bfh/bti7081/s2016/orange/mentalhealthcare/view/StartView.java
+++ src/main/java/ch/bfh/bti7081/s2016/orange/mentalhealthcare/view/StartView.java
@@ -4,45 +4,68 @@ import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.DateField;
+import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.ListSelect;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
 
-import ch.bfh.bti7081.s2016.orange.mentalhealthcare.controller.PatientController;
 import ch.bfh.bti7081.s2016.orange.mentalhealthcare.controller.StartController;
-import ch.bfh.bti7081.s2016.orange.mentalhealthcare.model.Patient;
 
 public class StartView extends VerticalLayout implements View {
-	public static final String NAME = "Create & Search";
+	public static final String NAME = "SearchPatients";
 
 	private static final long serialVersionUID = -4883635345472877648L;
 
 	private final StartController controller;
-	private final PatientController pcontroller;
 
 	public StartView() {
 		controller = new StartController();
-		pcontroller = new PatientController();
 
-		final Label title = new Label("Create & Search");
+		setMargin(true);
 
+		// Add title
+		final Label title = new Label("Search for Patients");
+		addComponent(title);
+
+		// Add content
+		final HorizontalLayout content = new HorizontalLayout();
+		content.setWidth("100%");
+		addComponent(content);
+
+		// Add a horizontal layout for the bottom part
+		final HorizontalLayout bottom = new HorizontalLayout();
+		addComponent(bottom);
+
+		// Add navigation and display sides
+		final VerticalLayout nav = new VerticalLayout();
+		nav.setWidth("250px");
+		final VerticalLayout display = new VerticalLayout();
+		display.setWidth("100%");
+		content.addComponents(nav, display);
+		content.setExpandRatio(display, 1);
+
+		// Add input fields
+		final VerticalLayout input = new VerticalLayout();
 		final TextField lastName = new TextField();
 		lastName.setCaption("Last name:");
-
 		final TextField firstName = new TextField();
 		firstName.setCaption("First name:");
-
 		final TextField assuranceNr = new TextField();
 		assuranceNr.setCaption("Social assurance number:");
-
 		final DateField birthDate = new DateField();
 		birthDate.setCaption("Birth date:");
+		input.addComponents(lastName, firstName, assuranceNr, birthDate);
+		input.setHeight("300px");
 
-		final ListSelect select = new ListSelect("Matching patients");
+		// Add output field
+		final ListSelect select = new ListSelect("Select patient:");
 		select.addItems(controller.getPatients());
 		select.setNullSelectionAllowed(false);
+		display.addComponents(select);
 
+		// Add search button
+		final VerticalLayout button = new VerticalLayout();
 		final Button searchButton = new Button("Search patient");
 		searchButton.addClickListener(e -> {
 			controller.searchPatient(lastName.getValue(), firstName.getValue(), assuranceNr.getValue(),
@@ -50,25 +73,19 @@ public class StartView extends VerticalLayout implements View {
 			select.removeAllItems();
 			select.addItems(controller.getPatients());
 		});
+		button.addComponent(searchButton);
+		button.setHeight("100px");
+
+		nav.addComponents(input, button);
 
 		final Button backButton = new Button("Return to main view");
 		backButton.addClickListener(e -> {
 			getUI().getNavigator().navigateTo(TestView.NAME);
 		});
-
-		final Button createButton = new Button("Create Patient");
-		createButton.addClickListener(e -> {
-			final Patient p = new Patient(lastName.getValue(), firstName.getValue(), assuranceNr.getValue(), 0,
-					birthDate.getValue());
-			pcontroller.create(p);
-		});
-
-		addComponents(title, lastName, firstName, assuranceNr, birthDate, createButton, searchButton, select,
-				backButton);
+		bottom.addComponent(backButton);
 	}
 
 	@Override
 	public void enter(ViewChangeEvent event) {
-		// TODO Auto-generated method stub
 	}
 }
