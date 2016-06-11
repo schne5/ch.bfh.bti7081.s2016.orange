6aa5bcbf93c7b548af393639ca9c33c890045a0c
kandr1 <rajina.kandiah@students.bfh.ch>
2016-06-11 11:38:26 +0200

    LoginController: comments hinzugefügt und in english

--- src/main/java/ch/bfh/bti7081/s2016/orange/mentalhealthcare/controller/LoginController.java
+++ src/main/java/ch/bfh/bti7081/s2016/orange/mentalhealthcare/controller/LoginController.java
@@ -1,23 +1,24 @@
 package ch.bfh.bti7081.s2016.orange.mentalhealthcare.controller;
 
-import ch.bfh.bti7081.s2016.orange.mentalhealthcare.model.Arzt;
+import ch.bfh.bti7081.s2016.orange.mentalhealthcare.model.Doctor;
 import ch.bfh.bti7081.s2016.orange.mentalhealthcare.persistence.ArztRepository;
 import ch.bfh.bti7081.s2016.orange.mentalhealthcare.persistence.ArztRepositoryFactory;
 
 public class LoginController {
-	Arzt a = new Arzt();
+	Doctor doctor = new Doctor();
 	ArztRepository repository;
 
 	public LoginController() {
 		repository = ArztRepositoryFactory.createJpaRepository();
 	}
 
-	public Arzt logIn(String username, String password) {
-		this.a = repository.findArztInfo(username, password);// (username,
-																// passwort)
-		if (a != null) {
-			if (username.equalsIgnoreCase(a.getUsername()) && password.equals(a.getPassword())) {
-				return a;
+	//Controll if the username and password are in database
+	public Doctor logIn(String username, String password) {
+		this.doctor = repository.findDoctortInfo(username, password);/
+		
+		if (doctor != null) {
+			if (username.equalsIgnoreCase(Doctor.getUsername()) && password.equals(doctor.getPassword())) {
+				return doctor;
 			}
 		}
 		return null;
