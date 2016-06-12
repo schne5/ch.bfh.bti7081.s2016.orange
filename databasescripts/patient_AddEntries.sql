60150e0d6a2002ddbedc238a2c08da783f40e6fd
schas2 <schas2@users.noreply.github.com>
2016-06-12 13:13:52 +0200

    Update persistence info and sql inserts

--- databasescripts/patient_AddEntries.sql
+++ databasescripts/patient_AddEntries.sql
@@ -1,5 +1,5 @@
-INSERT INTO patient(name, vorname, gebDatum, svNr, status) VALUES ('Rufer', 'Peter', STR_TO_DATE('01-05-1980','%d-%m-%Y'), '1000', 1);
-INSERT INTO patient(name, vorname, gebDatum, svNr, status) VALUES ('Mader', 'Marie', STR_TO_DATE('03-04-1966','%d-%m-%Y'), '2000', 1);
-INSERT INTO patient(name, vorname, gebDatum, svNr, status) VALUES ('Meyer', 'Leila', STR_TO_DATE('07-05-1982','%d-%m-%Y'), '3000', 1);
-INSERT INTO patient(name, vorname, gebDatum, svNr, status) VALUES ('Oesch', 'Hans', STR_TO_DATE('12-01-1952','%d-%m-%Y'), '4000', 1);
-INSERT INTO patient(name, vorname, gebDatum, svNr, status) VALUES ('Schlup', 'Jakob', STR_TO_DATE('21-09-1968','%d-%m-%Y'), '5000', 1);
+INSERT INTO patient(surename, firstname, birthdate, assuranceNr, state) VALUES ('Rufer', 'Peter', STR_TO_DATE('01-05-1980','%d-%m-%Y'), '1000', 1);
+INSERT INTO patient(surename, firstname, birthdate, assuranceNr, state) VALUES ('Mader', 'Marie', STR_TO_DATE('03-04-1966','%d-%m-%Y'), '2000', 1);
+INSERT INTO patient(surename, firstname, birthdate, assuranceNr, state) VALUES ('Meyer', 'Leila', STR_TO_DATE('07-05-1982','%d-%m-%Y'), '3000', 1);
+INSERT INTO patient(surename, firstname, birthdate, assuranceNr, state) VALUES ('Oesch', 'Hans', STR_TO_DATE('12-01-1952','%d-%m-%Y'), '4000', 1);
+INSERT INTO patient(surename, firstname, birthdate, assuranceNr, state) VALUES ('Schlup', 'Jakob', STR_TO_DATE('21-09-1968','%d-%m-%Y'), '5000', 1);
