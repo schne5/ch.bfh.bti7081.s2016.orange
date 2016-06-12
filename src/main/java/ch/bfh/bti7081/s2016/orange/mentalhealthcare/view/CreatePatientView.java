d706b68045fe7514dc89b9e32a58369c2e2fed01
kandr1 <rajina.kandiah@students.bfh.ch>
2016-06-12 15:45:58 +0200

    add Comments, correct notification to english

--- src/main/java/ch/bfh/bti7081/s2016/orange/mentalhealthcare/view/CreatePatientView.java
+++ src/main/java/ch/bfh/bti7081/s2016/orange/mentalhealthcare/view/CreatePatientView.java
@@ -7,8 +7,6 @@ import com.vaadin.ui.DateField;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
-import com.vaadin.ui.HorizontalLayout;
-import com.vaadin.ui.TabSheet;
 
 import ch.bfh.bti7081.s2016.orange.mentalhealthcare.controller.PatientController;
 import ch.bfh.bti7081.s2016.orange.mentalhealthcare.model.Patient;
@@ -18,68 +16,61 @@ public class CreatePatientView extends VerticalLayout implements View {
 
 	private final PatientController controller;
 	private Patient patient;
-	private Label hinweis = new Label();// Hinweis bei fehlerhafter eingabe
+	private Label hinweis = new Label();
 
 	public CreatePatientView() {
 		controller = new PatientController();
-		//final Label title = new Label("Create");
-
-		//TabSheet tabsheet = new TabSheet();
+		
+		// Add layout for Create
 		VerticalLayout layoutCreatePatient = getLayoutCreatePatient();
-		//tabsheet.addTab(layoutCreatePatient).setCaption("Create");
-
-		addComponents(getTop(), hinweis, layoutCreatePatient);
-
+		addComponents(hinweis, layoutCreatePatient);
 		setMargin(true);
-
 	}
-
-	private HorizontalLayout getTop() { // leer mal als Platzhalter drin
-		HorizontalLayout layoutTop = new HorizontalLayout();
-		layoutTop.addComponents();
-		layoutTop.setSpacing(true);
-		return layoutTop;
-	}
-
+	
+	//layout for Create
 	private VerticalLayout getLayoutCreatePatient() {
 		VerticalLayout layoutPatientenDaten = new VerticalLayout();
+		
+		// Add title for Create
 		final Label title = new Label("Create");
+		
+		// Add input fields
 		final TextField lastName = new TextField();
 		lastName.setCaption("Last name:");
-
 		final TextField firstName = new TextField();
 		firstName.setCaption("First name:");
-
 		final TextField assuranceNr = new TextField();
 		assuranceNr.setCaption("Social assurance number:");
-
 		final DateField birthDate = new DateField();
 		birthDate.setCaption("Birth date:");
-
-		final Button createButton = new Button("Create Patient");
+		
+		//Add create button
+		final Button createButton = new Button("Create patient");
 		createButton.addClickListener(e -> {
 			patient = new Patient();
 			patient.setSurename(firstName.getValue());
 			patient.setFirstname(firstName.getValue());
 			patient.setAssuranceNr(assuranceNr.getValue());
 			patient.setBirthdate(birthDate.getValue());
+			
+			// notification if there are no inputs in fields 
+			//if the Patientcreation was correct go to  StartView
 			if (controller.create(patient)) {
 				getUI().getNavigator().navigateTo(StartView.NAME);
 			} else {
 
-				this.hinweis.setCaption("Fehlerhafte Eingabe: Neuer Patient konnte nicht erstellt werden");
+				this.hinweis.setCaption("The input was incorrect: the new patient was not created. Please fill all fields");
 			}
 		});
-
+		
+		//Add back button
 		final Button startButton = new Button("Return to search view");
 		startButton.addClickListener(e -> {
 			getUI().getNavigator().navigateTo(StartView.NAME);
 		});
 
 		layoutPatientenDaten.addComponents(title, lastName, firstName, assuranceNr, birthDate, createButton, startButton);
-
 		layoutPatientenDaten.setSpacing(true);
-
 		return layoutPatientenDaten;
 	}
 
