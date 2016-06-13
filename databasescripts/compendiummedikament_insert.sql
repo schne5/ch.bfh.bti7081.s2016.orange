6a29019a177e4213ce83b04b947bfb1a77121b69
schne5 <elisa.schnabel@gmx.ch>
2016-06-13 16:43:54 +0200

    Compedndiummedikament Einträge sql hinzugefügt

---
+++ databasescripts/compendiummedikament_insert.sql
@@ -0,0 +1,5 @@
+INSERT INTO `compendiummedicament`( `name`) VALUES ("Citalopram");
+INSERT INTO `compendiummedicament`( `name`) VALUES ("Sertralin");
+INSERT INTO `compendiummedicament`( `name`) VALUES ("Fluoxetin");
+INSERT INTO `compendiummedicament`( `name`) VALUES ("Escitalopram");
+INSERT INTO `compendiummedicament`( `name`) VALUES ("Mirtazapin");
